4a58355fff463dc46471ebad75abe67416da5d60
cambell <cambell@localhost>
2012-03-13 15:34:06 +0700

    Add MergeHomographsConfig

--- src/LiftTools/Tools/MergeHomographs.cs
+++ src/LiftTools/Tools/MergeHomographs.cs
@@ -1,4 +1,11 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
 using Palaso.Code;
 using Palaso.Progress.LogBox;
 
@@ -6,7 +13,24 @@ namespace LiftTools.Tools
 {
     public class MergeHomographs : Tool
     {
+		public interface IConfigView
+		{
+			UserControl Control { get; }
+			void SetTraits(IEnumerable<string> traits);
+			IEnumerable<string> GetSelectedTraits();
+
+		}
+
+    	private IConfigView _config;
         private IProgress _progress;
+
+		public MergeHomographs()
+		{
+			var config = new MergeHomographsConfig(this);
+			_config = config;
+			ConfigControl = config;
+		}
+
         public override void Run(string inputLiftPath, string outputLiftPath, IProgress progress)
         {
             _progress = progress;
@@ -20,7 +44,6 @@ namespace LiftTools.Tools
 
             using (var repo = new Palaso.DictionaryServices.LiftLexEntryRepository(outputLiftPath))
             {
-
                 var writingSystemForMatching =   Palaso.DictionaryServices.Processors.HomographMerger.GuessPrimarLexicalFormWritingSystem(repo, progress);
                 if(writingSystemForMatching==null)
                 {
@@ -30,7 +53,7 @@ namespace LiftTools.Tools
                 progress.WriteMessage("");
                 progress.WriteMessage("Starting with {0} entries...", repo.Count);
                 progress.WriteMessage("");
-                Palaso.DictionaryServices.Processors.HomographMerger.Merge(repo, writingSystemForMatching, progress);
+                Palaso.DictionaryServices.Processors.HomographMerger.Merge(repo, writingSystemForMatching, _config.GetSelectedTraits().ToArray(), progress);
                 progress.WriteMessage("Ended with {0} entries...", repo.Count);
             }
             progress.WriteMessageWithColor("blue", "The processed lift is at " + outputLiftPath);
@@ -38,7 +61,7 @@ namespace LiftTools.Tools
             ValidateFile(progress, outputLiftPath);
         }
 
-        private void ValidateFile(IProgress progress, string path)
+        private static void ValidateFile(IProgress progress, string path)
         {
             progress.WriteMessage(""); 
             progress.WriteMessage("Validating the processed file...");
@@ -54,11 +77,47 @@ namespace LiftTools.Tools
             }
         }
 
+		public override void OnLiftFilePathChanged(string liftFilePath)
+		{
+			UpdateTraits(liftFilePath);
+		}
 
-        public override string ToString()
+    	private void UpdateTraits(string liftFilePath)
+    	{
+    		var traits = new HashSet<string>();
+    		using (var xmlReader = XmlReader.Create(new StreamReader(liftFilePath, Encoding.UTF8)))
+    		{
+    			while (xmlReader.Read())
+    			{
+    				if (xmlReader.NodeType == XmlNodeType.Element && xmlReader.LocalName == "entry")
+    				{
+    					var nodeReader = xmlReader.ReadSubtree();
+    					nodeReader.Read(); // The initial read
+    					try
+    					{
+    						var entryNode = XNode.ReadFrom(nodeReader) as XElement;
+    						var traitsInEntry = from node in entryNode.Descendants("trait")
+    						                    select node.Attribute("name").Value;
+    						foreach (var trait in traitsInEntry)
+    						{
+    							traits.Add(trait);
+    						}
+    					}
+    					catch (Exception e)
+    					{
+    						_progress.WriteMessageWithColor("red", "Couldn't process entry because: {0}", e.Message);
+    					}
+    				}
+    			}
+    		}
+    		_config.SetTraits(traits);
+    	}
+
+    	public override string ToString()
         {
             return "Merge Homographs";
         }
+
         public override string InfoPageName
         {
             get { return "MergeHomographs.htm"; }
---
+++ src/LiftTools/Tools/MergeHomographsConfig.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LiftTools.Tools
+{
+    public partial class MergeHomographsConfig : UserControl, MergeHomographs.IConfigView
+    {
+		private readonly MergeHomographs _presenter;
+
+		public MergeHomographsConfig(MergeHomographs presenter)
+		{
+			_presenter = presenter;
+            InitializeComponent();
+        }
+
+        public IEnumerable<string> CawlWritingSystemsToRemove()
+        {
+            var result = from object item in _clbMergeTraits.CheckedItems
+                         select item as string;
+            return result.ToList();
+        }
+
+    	public void SetCawlWritingSystems(IEnumerable<string> result)
+    	{
+			_clbMergeTraits.Items.Clear();
+			foreach (string writingSystem in result)
+			{
+				_clbMergeTraits.Items.Add(writingSystem);
+			}
+    	}
+
+    	public void SetDefaultCawlFilePath(string defaultCawlFilePath)
+    	{
+    		_dlgCawlFileChooser.InitialDirectory = Path.GetDirectoryName(defaultCawlFilePath);
+    		_dlgCawlFileChooser.FileName = Path.GetFileName(defaultCawlFilePath);
+    	}
+
+    	public UserControl Control
+    	{
+			get { return this; }
+    	}
+
+    	public void SetTraits(IEnumerable<string> traits)
+    	{
+			_clbMergeTraits.Items.Clear();
+			_clbMergeTraits.Items.AddRange(traits.ToArray());
+    	}
+
+    	public IEnumerable<string> GetSelectedTraits()
+    	{
+			var list = (from object item in _clbMergeTraits.CheckedItems select item as string);
+    		return list;
+    	}
+    }
+}
